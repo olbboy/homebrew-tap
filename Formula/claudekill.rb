e36dd629afea23fa358d983aae96a0a21f263630
mds <datdmict@gmail.com>
2026-01-05 01:59:06 +0700

    chore: update claudekill to v0.5.0

--- Formula/claudekill.rb
+++ Formula/claudekill.rb
@@ -1,13 +1,13 @@
 class Claudekill < Formula
   desc "A high-velocity, terminal-native instrument designed to hunt down and neutralize .claude cache directories"
   homepage "https://github.com/olbboy/claudekill"
-  version "0.1.1"
+  version "0.5.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/olbboy/claudekill/releases/download/v#{version}/claudekill-aarch64-apple-darwin.tar.gz"
-      sha256 "02512517ce5b880b0a1afef7e4af74993a4e269354c999967b5128aa5f7b1caf"
+      sha256 "926e8aa0c5c773f10decb04b84dd9fccb2d81256ca37a01269bb5c11d534c33a"
     end
     on_intel do
       url "https://github.com/olbboy/claudekill/releases/download/v#{version}/claudekill-x86_64-apple-darwin.tar.gz"
